b9ac550a33243a7bbb49a7968707163f08b507df
mike <viriyananda.michael@gmail.com>
2017-02-07 15:58:38 +0700

    Adding new module account_bank_statement_policy

---
+++ account_bank_statement_policy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Bank Statement Policy",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_accountant",
+    ],
+    "data": [
+        "views/account_journal_view.xml"
+    ],
+}
