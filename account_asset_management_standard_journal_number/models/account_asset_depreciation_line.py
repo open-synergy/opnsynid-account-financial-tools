97a2470081f278316badc3e882b6894ca23f967c
Andhitia Rama <andhitia.r@gmail.com>
2017-04-04 14:53:48 +0700

    [ADD][8.0] account_asset_management_standard_journal_number (#12)
    * [ADD][8.0] account_asset_management_standard_journal_number
    
    * Add OCA/account-financial-tools
    
    * Fix PEP8

---
+++ account_asset_management_standard_journal_number/models/account_asset_depreciation_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class AccountAssetDepreciationLine(models.Model):
+    _inherit = "account.asset.depreciation.line"
+
+    def _setup_move_data(
+            self, depreciation_line, depreciation_date,
+            period_id, context):
+        move_data = super(AccountAssetDepreciationLine, self)._setup_move_data(
+            depreciation_line, depreciation_date,
+            period_id, context)
+        move_data["name"] = "/"
+        return move_data
