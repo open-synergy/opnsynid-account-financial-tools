311cac1fc5e76ae3a9d3ce162e104dbb1a3844ea
mike <viriyananda.michael@gmail.com>
2017-08-02 07:41:08 +0700

    Revisi Unittest

--- account_journal_bank_cash_create_menu/tests/base.py
+++ account_journal_bank_cash_create_menu/tests/base.py
@@ -17,3 +17,37 @@ class TestBaseCreateMenu(TransactionCase):
         # Data
         self.bank_journal =\
             self.env.ref('account.bank_journal')
+        self.search_bank_view =\
+            self.env.ref('account.view_bank_statement_search')
+        self.tree_bank_view =\
+            self.env.ref('account.view_bank_statement_tree')
+        self.from_bank_view =\
+            self.env.ref('account.view_bank_statement_form')
+        self.parent_menu =\
+            self.env.ref('account.account_account_menu')
+
+    def _create_menu(self, parent_menu_id=False):
+        new = self.wiz.with_context(
+            active_model="account.journal",
+            active_ids=[self.bank_journal.id]
+        ).new()
+
+        new.parent_menu_id = parent_menu_id
+        new.menu_name = self.bank_journal.name
+        new.search_view_id = self.search_bank_view.id
+        new.view_ids = [
+            (0, 0, {
+                'sequence': 1,
+                'view_id': self.tree_bank_view.id,
+                'view_mode': 'tree'
+                }),
+            (0, 0, {
+                'sequence': 2,
+                'view_id': self.from_bank_view.id,
+                'view_mode': 'form'
+                })
+        ]
+
+        # Create Menu
+        new.button_create_menu()
+        return True
