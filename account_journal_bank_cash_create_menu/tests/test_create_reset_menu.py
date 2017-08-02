930b9d85a6e503b590fb28cff95deb02cce2e89a
mike <viriyananda.michael@gmail.com>
2017-08-02 07:41:08 +0700

    Revisi Unittest

--- account_journal_bank_cash_create_menu/tests/test_create_reset_menu.py
+++ account_journal_bank_cash_create_menu/tests/test_create_reset_menu.py
@@ -6,15 +6,8 @@ from .base import TestBaseCreateMenu
 
 
 class TestCreateResetMenu(TestBaseCreateMenu):
-    def test_create_reset_menu(self):
-        new = self.wiz.with_context(
-            active_model="account.journal",
-            active_ids=[self.bank_journal.id]
-        ).new()
-
-        new.menu_name = self.bank_journal.name
-        # Create Menu
-        new.button_create_menu()
+    def test_create_menu(self):
+        self._create_menu()
 
         # Check menu_id & window_action_id
         # Condition : menu_id or window_action_id == False
@@ -33,6 +26,69 @@ class TestCreateResetMenu(TestBaseCreateMenu):
             True,
             result_window_action_id
         )
+
+    def test_create_menu_2(self):
+        self._create_menu()
+
+        # Check menu_id & window_action_id
+        # Condition : menu_id or window_action_id == False
+        result_menu_id = False
+        result_window_action_id = False
+
+        if self.bank_journal.menu_id.id:
+            result_menu_id = True
+            menu_id = self.bank_journal.menu_id.id
+        self.assertEqual(
+            True,
+            result_menu_id
+        )
+        if self.bank_journal.window_action_id.id:
+            result_window_action_id = True
+            waction_id =\
+                self.bank_journal.window_action_id.id
+        self.assertEqual(
+            True,
+            result_window_action_id
+        )
+
+        # Check menu_id & window_action_id
+        # Condition : menu_id or window_action_id == True
+        self._create_menu()
+
+        self.assertEqual(
+            self.bank_journal.menu_id.id,
+            menu_id
+        )
+        self.assertEqual(
+            self.bank_journal.window_action_id.id,
+            waction_id
+        )
+
+    def test_create_menu_3(self):
+        # Create Menu With Parent Menu ID
+
+        self._create_menu(self.parent_menu.id)
+
+        # Check menu_id & window_action_id
+        # Condition : menu_id or window_action_id == False
+        result_menu_id = False
+        result_window_action_id = False
+
+        if self.bank_journal.menu_id.id:
+            result_menu_id = True
+        self.assertEqual(
+            True,
+            result_menu_id
+        )
+        if self.bank_journal.window_action_id.id:
+            result_window_action_id = True
+        self.assertEqual(
+            True,
+            result_window_action_id
+        )
+
+    def test_reset_menu(self):
+        self._create_menu()
 
         # Pressing Reset Button
         self.bank_journal.button_reset_menu()
