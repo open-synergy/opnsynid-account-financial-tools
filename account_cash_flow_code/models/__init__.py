4c80f6054ee75d3a792760dca446f61edfccbaaa
Andhitia Rama <andhitia.r@gmail.com>
2017-12-04 15:55:02 +0700

    [8.0.1.0.0][account_cash_flow_code] QC + Rev

--- account_cash_flow_code/models/__init__.py
+++ account_cash_flow_code/models/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import(
-	account_cash_flow_code,
-	account_account,
-	account_move_line
+    account_cash_flow_code,
+    account_account,
+    account_move_line
 )
