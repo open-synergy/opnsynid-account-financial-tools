2a2ab73fb6df6b5851235213cf8dec666000f20b
Andhitia Rama <andhitia.r@gmail.com>
2018-09-30 16:11:19 +0700

    [8.0.1.0.0] account_asset_management_config_page

---
+++ account_asset_management_config_page/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Asset Management Configuration Page",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "views/account_asset_config_setting_views.xml",
+    ]
+}
