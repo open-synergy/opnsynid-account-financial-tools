e55117840bd3fbc9e15f0aa0fdb6920c0ec27f23
Andhitia Rama <andhitia.r@gmail.com>
2019-03-17 16:47:02 +0700

    [8.0.1.0.0] account_move_back_forward_date_policy

---
+++ account_move_back_forward_date_policy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Account Move Back & Forward Date Policy",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_journal_views.xml",
+    ]
+}
