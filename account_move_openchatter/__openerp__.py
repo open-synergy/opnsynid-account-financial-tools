38bcaa3e782bfc1662bc368687bf4f11607cbce6
Andhitia Rama <andhitia.r@gmail.com>
2020-09-22 15:15:09 +0700

    [account_move_openchatter] 8.0.1.0.0

---
+++ account_move_openchatter/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Move Open Chatter",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
