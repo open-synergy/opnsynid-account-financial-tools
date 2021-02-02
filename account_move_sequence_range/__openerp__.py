9095bece3f4fb41524d511aa56b771b375adfbef
Andhitia Rama <andhitia.r@gmail.com>
2021-02-03 06:32:55 +0700

    [8.0.1.0.0] account_move_sequence_range

---
+++ account_move_sequence_range/__openerp__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Move Sequence Range",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account",
+        "sequence_range",
+    ],
+}
