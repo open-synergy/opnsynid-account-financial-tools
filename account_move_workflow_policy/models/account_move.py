4b4aae5b773f4a28b73865b52e1e74760c6ce8f4
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-08 14:12:54 +0700

    [account_move_workflow_policy] 11.0.1.0.0

---
+++ account_move_workflow_policy/models/account_move.py
@@ -0,0 +1,40 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _name = "account.move"
+    _inherit = [
+        "account.move",
+        "mixin.policy",
+    ]
+
+    @api.multi
+    def _compute_policy(self):
+        _super = super(AccountMove, self)
+        _super._compute_policy()
+
+    # Policy Field
+    post_ok = fields.Boolean(
+        string="Can Post",
+        compute="_compute_policy",
+    )
+    duplicate_ok = fields.Boolean(
+        string="Can Duplicate",
+        compute="_compute_policy",
+    )
+    reverse_ok = fields.Boolean(
+        string="Can Reverse Entry",
+        compute="_compute_policy",
+    )
+
+    @api.onchange(
+        "journal_id",
+    )
+    def onchange_policy_template_id(self):
+        template_id = self._get_template_id()
+        for document in self:
+            document.policy_template_id = template_id
