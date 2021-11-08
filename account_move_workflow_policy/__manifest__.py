d5dfd657135ada80d17c59b798de036c6d989ebb
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-08 14:12:54 +0700

    [account_move_workflow_policy] 11.0.1.0.0

---
+++ account_move_workflow_policy/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Account Move - Workflow Policy",
+    "version": "11.0.1.0.0",
+    "category": "Administration",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+        "ssi_policy_mixin",
+    ],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+}
