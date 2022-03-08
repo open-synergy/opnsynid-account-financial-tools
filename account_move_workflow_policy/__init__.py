1dc9bcfa92be984f5e79fcfa4d9fc2b2e31bf0ec
mikevhe18 <viriyananda.michael@gmail.com>
2022-03-08 10:16:08 +0700

    [account_move_workflow_policy] 11.0.1.1.0 QC

--- account_move_workflow_policy/__init__.py
+++ account_move_workflow_policy/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2021 OpenSynergy Indonesia
-# Copyright 2021 PT. Simetri Sinergi Indonesia
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
