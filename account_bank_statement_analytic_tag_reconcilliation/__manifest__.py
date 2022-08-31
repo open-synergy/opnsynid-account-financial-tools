81dd3b0c3510da00fcba43bbab62cdd72f33b642
Heri <herixr4@gmail.com>
2022-08-31 21:44:19 +0700

    [account_bank_statement_analytic_tag_reconcilliation] 11.0.1.0.1 Only show analytic tags in bank statement

--- account_bank_statement_analytic_tag_reconcilliation/__manifest__.py
+++ account_bank_statement_analytic_tag_reconcilliation/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Reconcilliation Analytic",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia",
