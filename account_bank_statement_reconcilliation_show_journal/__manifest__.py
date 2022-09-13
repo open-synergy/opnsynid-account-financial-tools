54dadebedfee7afc0e7b588de324838869f1d3ad
Andhitia Rama <andhitia.r@gmail.com>
2022-09-14 04:25:10 +0700

    [account_bank_statement_reconcilliation_show_journal] 11.0.1.0.0

---
+++ account_bank_statement_reconcilliation_show_journal/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Show Journal on Reconcilliation Widget",
+    "version": "11.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account"],
+    "data": [
+        # "views/assets_backend.xml",
+    ],
+    "qweb": [
+        "static/src/xml/account_reconciliation.xml",
+    ],
+    "demo": [],
+    "images": [],
+}
