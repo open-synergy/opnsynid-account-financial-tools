4bef4569ab8b7f33d7273cc36ca35fc20367afe1
mikevhe18 <viriyananda.michael@gmail.com>
2023-01-25 14:24:12 +0700

    [ssi_analytic_account_mass_assign] 11.0.1.0.0

---
+++ ssi_analytic_account_mass_assign/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Analytic Account Mass Assign",
+    "version": "11.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "wizards/account_analytic_mass_assign_view.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_analytic_account_mass_assign/wizards/account_analytic_mass_assign.py
@@ -0,0 +1,26 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import api, fields, models
+
+
+class AccountAnalyticMassAssign(models.TransientModel):
+    _name = "account.analytic_mass_assign"
+    _description = "Account Analytic Mass Assign"
+
+    analytic_account_id = fields.Many2one(
+        string="Analytic Account",
+        comodel_name="account.analytic.account",
+        required=True,
+    )
+
+    @api.multi
+    def button_change_analytic(self):
+        obj_acc_move_line = self.env["account.move.line"]
+        active_ids = self.env.context["active_ids"]
+
+        for record in obj_acc_move_line.browse(active_ids):
+            record.analytic_account_id = self.analytic_account_id.id
+            record.create_analytic_lines()
+
+        return True
