8b0beed416cbf3bc9814d6070702d198bc5d32a9
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-21 20:39:38 +0700

    [ssi_analytic_account_mass_assign] Change License To AGPL-3

--- ssi_analytic_account_mass_assign/__manifest__.py
+++ ssi_analytic_account_mass_assign/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Account Mass Assign",
     "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_financial_accounting",
